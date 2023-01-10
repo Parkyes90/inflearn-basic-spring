9488e248fd333e2ef3be0a16b1f09d1fb8a6c537
parkyes90 <parkyes90@gmail.com>
2023-01-10 21:52:52 +0900

    Modify test member id

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -6,8 +6,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class RateDiscountPolicyTest {
 
     RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
@@ -28,7 +26,7 @@ class RateDiscountPolicyTest {
     @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다.")
     void vip_x() {
         // given
-        Member member = new Member(1L, "memberVIP", Grade.BASIC);
+        Member member = new Member(2L, "memberVIP", Grade.BASIC);
         // when
         int discount = discountPolicy.discount(member, 10000);
         // then
