9ff006917c56dd1ee01aeff0b5e0fef1cc98c127
parkyes90 <parkyes90@gmail.com>
2023-01-19 23:21:05 +0900

    Add configurationDeep Test

--- src/test/java/hello/core/singleton/ConfigurationsSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationsSingletonTest.java
@@ -27,4 +27,12 @@ public class ConfigurationsSingletonTest {
 
         Assertions.assertThat(memberRepository1).isSameAs(memberRepository2).isSameAs(memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+    }
 }
