e065fc1d6b976369a9a921c8eb8f2f3cf2c2a467
parkyes90 <parkyes90@gmail.com>
2023-01-20 21:18:47 +0900

    Add ComponentFilterAppConfigTest

---
+++ src/test/java/hello/core/scan/filter/MyIncludeComponent.java
@@ -0,0 +1,11 @@
+package hello.core.scan.filter;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target(java.lang.annotation.ElementType.TYPE)
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyIncludeComponent {
+}
